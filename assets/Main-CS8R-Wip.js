03de10beb350550910ad3a277ae5906606bf11ac
Yauheni <yauhenikozak@gmail.com>
2024-09-01 15:15:11 +0300

    Updates

--- assets/Main-CS8R-Wip.js
+++
@@ -1,1 +0,0 @@
-import{d as s,c as t,a,b as n,t as r,u as c,r as i,o as _}from"./index-CEZW6J-y.js";const l={class:"flex grow flex-col w-full main-container"},p={class:"fixed bottom-4 right-4"},h=s({__name:"Main",setup(m){const e="0.0.10";return(d,f)=>{const o=i("router-view");return _(),t("div",l,[a(o,{class:"h-[100vh]"}),n("span",p,r(c(e)),1)])}}});export{h as default};
