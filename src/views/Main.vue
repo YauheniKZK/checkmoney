b382a67296a67419b4f213cc69491e9bfec3fa5e
Yauheni <yauhenikozak@gmail.com>
2024-09-01 12:53:03 +0300

    fix

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+const versionApp = import.meta.env.PACKAGE_VERSION
 </script>
 
 <template>
   <div class="flex grow flex-col w-full main-container">
     <router-view class="h-[100vh]" />
+    <span class="fixed bottom-4 right-4">{{ versionApp }}</span>
   </div>
 </template>
 
