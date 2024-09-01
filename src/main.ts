d4124ea0d132be927ef7ac225acae6f0d27aaf15
Yauheni <yauhenikozak@gmail.com>
2024-09-01 11:05:42 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import './assets/style.css'
 import App from './App.vue'
+import naive from 'naive-ui'
+import router from './router'
 
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
 app.use(createPinia())
-// app.use(router)
+app.use(router)
+app.use(naive)
 
 app.mount('#app')
