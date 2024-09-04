e09d3782a5d3dd43457149f664bfe3dd8b126f17
Yauheni <yauhenikozak@gmail.com>
2024-09-04 11:37:27 +0300

    Updates

---
+++ assets/Main-BuVECoer.js
@@ -0,0 +1,1 @@
+import{d as s,c as t,a,b as n,t as r,u as c,r as i,o as _}from"./index-BImqHhQx.js";const l={class:"flex grow flex-col w-full main-container"},p={class:"fixed bottom-4 right-4"},h=s({__name:"Main",setup(m){const e="0.0.16";return(d,f)=>{const o=i("router-view");return _(),t("div",l,[a(o,{class:"h-[100vh]"}),n("span",p,r(c(e)),1)])}}});export{h as default};
