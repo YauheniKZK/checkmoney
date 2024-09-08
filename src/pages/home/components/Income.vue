8b0cc79f66b10764d7581c57c51a1739080a8204
Yauheni <yauhenikozak@gmail.com>
2024-09-08 13:13:45 +0300

    fix

--- src/pages/home/components/Income.vue
+++ src/pages/home/components/Income.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { MoneyCalculator20Regular } from '@vicons/fluent'
 </script>
 
 <template>
-  <div class="flex flex-col w-full">
-   {{ 'income' }}
+  <div class="flex flex-col w-full grow">
+    <div class="flex flex-col justify-center items-center grow">
+      <n-icon :size="128" :color="'#eeeeee'" class="mb-4">
+        <MoneyCalculator20Regular />
+      </n-icon>
+      <span class="text-[#eeeeee]">{{'Скоро...'}}</span>
+    </div>
   </div>
 </template>
 
