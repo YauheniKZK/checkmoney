4d99e524b846c9aa8fd0e2ea6826fe2a710dbfed
Yauheni <yauhenikozak@gmail.com>
2024-09-08 13:33:36 +0300

    Updates

--- assets/Settings-sq9jnZd_.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,c as s,a as o,t as c}from"./index-9zLaafVJ.js";const n={},a={class:"flex flex-col w-full pt-4"},_=o("span",null,c("Settings"),-1),l=[_];function r(i,f){return e(),s("div",a,l)}const p=t(n,[["render",r]]);export{p as default};
